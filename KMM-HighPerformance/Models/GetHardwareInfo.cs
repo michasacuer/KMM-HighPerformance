276f419c26353f31766d898f2a6e548dc8670e28
Michał Szewczak <michalszewczak2@gmail.com>
2018-08-21 11:47:23 +0200

    Working on hardware info

---
+++ KMM-HighPerformance/Models/GetHardwareInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace KMM_HighPerformance.Models
+{
+    class GetHardwareInfo
+    {
+
+        //work in progress
+        static public void GetCPU()
+        {
+            //var mbs = new ManagementObjectSearcher("Select ProcessorID From Win32_processor");
+            //var mbsList = mbs.Get();
+            //
+            //foreach (ManagementObject mo in mbsList)
+            //{
+            //    var cpuid = mo["ProcessorID"].ToString();
+            //
+            //}
+
+
+        }
+
+    }
+}
