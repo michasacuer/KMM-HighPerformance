90026dc0d9720d35f86900fc2b3c5408f39058bc
Michał Szewczak <michalszewczak2@gmail.com>
2018-09-12 21:21:45 +0200

    refactor

--- KMM-HighPerformance/Functions/Algorithms/KMMHighPerformancemMain.cs
+++ KMM-HighPerformance/Functions/Algorithms/KMMHighPerformancemMain.cs
@@ -10,58 +10,39 @@ namespace KMM_HighPerformance.Algorithms
 {
     class KMMHighPerformanceMain
     {
-        static public Bitmap Init(Bitmap tempBmp, MeasureTime measure)
+        static public unsafe Bitmap Init(Bitmap tempBmp, MeasureTime measure)
         {
             var stopwatch = Stopwatch.StartNew();
-
             tempBmp = BitmapConversion.Create8bppGreyscaleImage(tempBmp);
 
-            unsafe
+            BitmapData bmpData = tempBmp.LockBits(new Rectangle(0, 0, 
+                                                  tempBmp.Width, 
+                                                  tempBmp.Height), 
+                                                  ImageLockMode.ReadWrite, 
+                                                  tempBmp.PixelFormat
+                                                  );
+            
+            int bytes = bmpData.Stride * tempBmp.Height;
+            byte[] pixels = new byte[bytes];
+            Marshal.Copy(bmpData.Scan0, pixels, 0, bytes);
+            int height = tempBmp.Height;
+            int width = tempBmp.Width;              
+
+            int deletion = 1;
+            while (deletion != 0)
             {
-                BitmapData bmpData = tempBmp.LockBits(new Rectangle(0, 0, 
-                                                      tempBmp.Width, 
-                                                      tempBmp.Height), 
-                                                      ImageLockMode.ReadWrite, 
-                                                      tempBmp.PixelFormat
-                                                      );
-                
-                int bytes = bmpData.Stride * tempBmp.Height;
-                byte[] pixels = new byte[bytes];
-
-                Marshal.Copy(bmpData.Scan0, pixels, 0, bytes);
-
-                int height = tempBmp.Height;
-                int width = tempBmp.Width;              
-
-                int deletion = 1;
-                while (deletion != 0)
-                {
-                    deletion = 0;
-                    pixels = HighPerformance.SetTwoThree(pixels, 
-                                                         bmpData, 
-                                                         height, 
-                                                         width);
-
-                    (deletion, pixels) = HighPerformance.FindAndDeleteFour(pixels, 
-                                                                           bmpData, 
-                                                                           height, 
-                                                                           width, 
-                                                                           deletion);
-
-                    (deletion, pixels) = HighPerformance.DeletingTwoThree(pixels, 
-                                                                          bmpData, 
-                                                                          height, 
-                                                                          width, 
-                                                                          deletion);
-                }
-
-                Marshal.Copy(pixels, 0, bmpData.Scan0, bytes);
-                tempBmp.UnlockBits(bmpData);
+                deletion = 0;
+                pixels = HighPerformance.SetTwoThree(pixels, bmpData, height, width);
+                (deletion, pixels) = HighPerformance.FindAndDeleteFour(pixels, bmpData, height, width, deletion);
+                (deletion, pixels) = HighPerformance.DeletingTwoThree(pixels, bmpData, height, width, deletion);
             }
+
+            Marshal.Copy(pixels, 0, bmpData.Scan0, bytes);
+            tempBmp.UnlockBits(bmpData);
+
             measure.SumTimeElapsed(stopwatch.ElapsedMilliseconds);
             return tempBmp;
         }
     }
-
 }
 
